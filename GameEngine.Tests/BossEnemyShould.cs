982afb91e0e61aa6d7f438453213318746563647
Vitali Prakapenka <myitvit@gmail.com>
2022-11-24 12:48:49 +0300

    Asserting on Floating Point Values

---
+++ GameEngine.Tests/BossEnemyShould.cs
@@ -0,0 +1,14 @@
+﻿namespace GameEngine.Tests
+{
+    public class BossEnemyShould
+    {
+
+        [Fact]
+        public void HaveCorrectPower()
+        {
+            BossEnemy sut = new BossEnemy();
+
+            Assert.Equal(166.667, sut.TotalSpecialAttackPower, 3);
+        }
+    }
+}
