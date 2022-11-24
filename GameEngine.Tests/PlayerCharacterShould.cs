7ef90a843fc8339a99ce53aecbdc47feee58b381
Vitali Prakapenka <myitvit@gmail.com>
2022-11-24 11:12:13 +0300

    Assertions Against String Values

--- GameEngine.Tests/PlayerCharacterShould.cs
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -9,4 +9,70 @@ public class PlayerCharacterShould
 
         Assert.True(sut.IsNoob);
     }
+
+    [Fact]
+    public void CalculateFullName()
+    {
+        PlayerCharacter sut = new PlayerCharacter();
+
+        sut.FirstName = "Sarah";
+        sut.LastName = "Smith";
+
+        Assert.Equal("Sarah Smith", sut.FullName);
+    }
+
+    [Fact]
+    public void HaveFullNameStartingWithFirstName()
+    {
+        PlayerCharacter sut = new PlayerCharacter();
+
+        sut.FirstName = "Sarah";
+        sut.LastName = "Smith";
+
+        Assert.StartsWith("Sarah", sut.FullName);
+    }
+
+    [Fact]
+    public void HaveFullNameEndingWithLastName()
+    {
+        PlayerCharacter sut = new PlayerCharacter();
+
+        sut.FirstName = "Sarah";
+        sut.LastName = "Smith";
+
+        Assert.EndsWith("Smith", sut.FullName);
+    }
+
+    [Fact]
+    public void CalculateFullName_IgnoreCaseAssertExample()
+    {
+        PlayerCharacter sut = new PlayerCharacter();
+
+        sut.FirstName = "SARAH";
+        sut.LastName = "SMITH";
+
+        Assert.Equal("Sarah Smith", sut.FullName, ignoreCase: true);
+    }
+
+    [Fact]
+    public void CalculateFullName_SubsringAssertExample()
+    {
+        PlayerCharacter sut = new PlayerCharacter();
+
+        sut.FirstName = "Sarah";
+        sut.LastName = "Smith";
+
+        Assert.Contains("ah Sm", sut.FullName);
+    }
+
+    [Fact]
+    public void CalculateFullNameWithTitleCase()
+    {
+        PlayerCharacter sut = new PlayerCharacter();
+
+        sut.FirstName = "Sarah";
+        sut.LastName = "Smith";
+
+        Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", sut.FullName);
+    }
 }
