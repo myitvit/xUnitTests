76c5d2816567ed05c7c3b4c80d9633b5c60f26d6
Vitali Prakapenka <myitvit@gmail.com>
2022-11-28 14:16:47 +0300

    Sharing Context Across Test Classes

---
+++ GameEngine.Tests/TestClass1.cs
@@ -0,0 +1,30 @@
+﻿using Xunit.Abstractions;
+
+namespace GameEngine.Tests
+{
+    [Collection("GameState collection")]
+    public class TestClass1
+    {
+        private readonly GameStateFixture gameStateFixture;
+        private readonly ITestOutputHelper output;
+
+        public TestClass1(GameStateFixture gameStateFixture, ITestOutputHelper output)
+        {
+            this.gameStateFixture = gameStateFixture;
+
+            this.output = output;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            output.WriteLine($"GameState ID={gameStateFixture.State.Id}");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            output.WriteLine($"GameState ID={gameStateFixture.State.Id}");
+        }
+    }
+}
---
+++ GameEngine.Tests/TestClass2.cs
@@ -0,0 +1,30 @@
+﻿using Xunit.Abstractions;
+
+namespace GameEngine.Tests
+{
+    [Collection("GameState collection")]
+    public class TestClass2
+    {
+        private readonly GameStateFixture gameStateFixture;
+        private readonly ITestOutputHelper output;
+
+        public TestClass2(GameStateFixture gameStateFixture, ITestOutputHelper output)
+        {
+            this.gameStateFixture = gameStateFixture;
+
+            this.output = output;
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            output.WriteLine($"GameState ID={gameStateFixture.State.Id}");
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            output.WriteLine($"GameState ID={gameStateFixture.State.Id}");
+        }
+    }
+}
