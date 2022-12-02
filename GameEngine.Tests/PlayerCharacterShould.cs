c4baa24efa9d1a6fbde87c4828c2cc92f0cf5ee5
Vitali Prakapenka <myitvit@gmail.com>
2022-12-02 12:42:24 +0300

    Refactoring to Data-driven Tests

--- GameEngine.Tests/PlayerCharacterShould.cs
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -189,40 +189,16 @@ namespace GameEngine.Tests
 
         #endregion
 
-        #region 5. 03. Adding Extra PlayerCharacter Tests
-
-        [Fact]
-        public void TakeZeroDamage()
+        [Theory]
+        [InlineData(0, 100)]
+        [InlineData(1, 99)]
+        [InlineData(50, 50)]
+        [InlineData(101, 1)]
+        public void TakeDamage(int damage, int expectedHealth)
         {
-            sut.TakeDamage(0);
+            sut.TakeDamage(damage);
 
-            Assert.Equal(100, sut.Health);
+            Assert.Equal(expectedHealth, sut.Health);
         }
-
-        [Fact]
-        public void TakeSmallDamage()
-        {
-            sut.TakeDamage(1);
-
-            Assert.Equal(99, sut.Health);
-        }
-
-        [Fact]
-        public void TakeMediumDamage()
-        {
-            sut.TakeDamage(50);
-
-            Assert.Equal(50, sut.Health);
-        }
-
-        [Fact]
-        public void TakeMinimum1Health()
-        {
-            sut.TakeDamage(101);
-
-            Assert.Equal(1, sut.Health);
-        }
-
-        #endregion
     }
 }
