46110ae31ea669922e14dcdb46bb3d724144fd40
Vitali Prakapenka <myitvit@gmail.com>
2022-12-03 14:30:20 +0300

    Getting Test Data from External Sources

--- GameEngine.Tests/NonPlayerCharacterShould.cs
+++ GameEngine.Tests/NonPlayerCharacterShould.cs
@@ -5,7 +5,7 @@ namespace GameEngine.Tests
     public class NonPlayerCharacterShould
     {
         [Theory]
-        [MemberData(nameof(InternalHealthDamageTestData.TestData), MemberType = typeof(InternalHealthDamageTestData))]
+        [MemberData(nameof(ExternalHealthDamageTestData.TestData), MemberType = typeof(ExternalHealthDamageTestData))]
         public void TakeDamage(int damage, int expectedHealth)
         {
             NonPlayerCharacter sut = new NonPlayerCharacter();
